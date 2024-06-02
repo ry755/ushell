147fdde2ca1bbdcefcec36fc93afd39fa8fe8bfa
Ry <ryanst755@gmail.com>
2024-06-02 00:31:35 -0700

    Move application SRAM start to 0x0800

--- tests/build.sh
+++ tests/build.sh
@@ -4,15 +4,15 @@ mkdir -p ../bin/ushell/
 
 function build_asm {
     avr-as -mmcu=atmega644 $1.s -o $1.elf
-    avr-ld -Map $1.map -T ushell.ld -o $1.out $1.elf
+    avr-ld -Map $1.map -T ../applications/ushell.ld -o $1.out $1.elf
     avr-objcopy -O binary $1.out $1.bin
-    cp $1.bin ../bin/ushell/
+    cp $1.bin ../bin/
 }
 
 function build_c {
-    avr-gcc -mmcu=atmega644 -DF_CPU=28636360UL -Os -T ushell.ld startup.s $1.c ushell.c -o $1.elf
+    avr-gcc -mmcu=atmega644 -DF_CPU=28636360UL -Os -T ../applications/ushell.ld startup.s $1.c ushell.c -o $1.elf
     avr-objcopy -O binary $1.elf $1.bin
-    cp $1.bin ../bin/ushell/
+    cp $1.bin ../bin/
 }
 
 build_asm test
