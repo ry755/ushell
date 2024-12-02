15a0a15a24830c651c36764daf5e4d0e501e13ab
Ry <ryanst755@gmail.com>
2024-12-02 01:28:31 -0800

    Fix test apps

--- tests/build.sh
+++ tests/build.sh
@@ -3,14 +3,14 @@
 mkdir -p ../bin/ushell/
 
 function build_asm {
-    avr-as -mmcu=atmega644 $1.s -o $1.elf
+    avr-as -mmcu=atmega644 ../applications/startup.s $1.s -o $1.elf
     avr-ld -Map $1.map -T ../applications/ushell.ld -o $1.out $1.elf
     avr-objcopy -O binary $1.out $1.app
     cp $1.app ../bin/
 }
 
 function build_c {
-    avr-gcc -mmcu=atmega644 -DF_CPU=28636360UL -Os -T ../applications/ushell.ld startup.s $1.c ushell.c -o $1.elf
+    avr-gcc -mmcu=atmega644 -DF_CPU=28636360UL -Os -T ../applications/ushell.ld ../applications/startup.s $1.c ushell.c -o $1.elf
     avr-objcopy -O binary $1.elf $1.app
     cp $1.app ../bin/
 }
